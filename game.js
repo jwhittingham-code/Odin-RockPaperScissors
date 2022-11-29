f4b52b9f1816df269ddb6440d5aa0a3bbb2961eb
jwhittingham-code <jwhittingham002@gmail.com>
2022-11-29 17:10:34 +1100

    created blank html doc with script tag for js file created js file and wrote in pesudo code

---
+++ game.js
@@ -0,0 +1,14 @@
+//Scissors Paper Rock
+// Randomly generate computers choice - function
+    //set variable with function that generates random number between 0 - 2
+    //set choice based on number generated
+// get players choice - function
+    //get input
+    //set it to lower case
+    //check it matches one of the three choices
+    //assign choice
+//compare the two - function
+    // using a switch statement compare choices
+//decide winner - game funcion
+    // based on above comparison
+//call above functions in a new function called "game"
